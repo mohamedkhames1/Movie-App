91e83839b106ee3053c6d4f2027b3fbc1ec41fe7
Mohamedkhames1 <moham01555@gmail.com>
2025-06-26 21:46:47 +0300

    upload Stracture of app

---
+++ lib/core/utils/assets.dart
@@ -0,0 +1,41 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsImagesEmpty
+  /// assets/images/empty.svg
+  static const String assetsImagesEmpty = "assets/images/empty.svg";
+
+  /// Assets for assetsImagesNoResults
+  /// assets/images/no-results.svg
+  static const String assetsImagesNoResults = "assets/images/no-results.svg";
+
+  /// Assets for assetsImagesOnboarding1
+  /// assets/images/onboarding_1.png
+  static const String assetsImagesOnboarding1 = "assets/images/onboarding_1.png";
+
+  /// Assets for assetsImagesOnboarding2
+  /// assets/images/Onboarding_2.png
+  static const String assetsImagesOnboarding2 = "assets/images/Onboarding_2.png";
+
+  /// Assets for assetsImagesOnboarding3
+  /// assets/images/onboarding_3.png
+  static const String assetsImagesOnboarding3 = "assets/images/onboarding_3.png";
+
+  /// Assets for assetsImagesSplash
+  /// assets/images/splash.png
+  static const String assetsImagesSplash = "assets/images/splash.png";
+
+  /// Assets for assetsImagesSplash12
+  /// assets/images/splash_12.png
+  static const String assetsImagesSplash12 = "assets/images/splash_12.png";
+
+  /// Assets for assetsImagesTestImage
+  /// assets/images/test_image.png
+  static const String assetsImagesTestImage = "assets/images/test_image.png";
+
+  /// Assets for assetsImagesVideo
+  /// assets/images/Video.png
+  static const String assetsImagesVideo = "assets/images/Video.png";
+}
+
---
+++ lib/core/utils/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color primary = Color(0xFF12CDD9);
+  static const Color secondary = Colors.orange;
+  static const Color categoryColor = Color(0xFF88A4E8);
+  static const Color background = Color(0xFF1F1D2B);
+}
