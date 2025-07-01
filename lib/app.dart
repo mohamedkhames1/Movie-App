982ba51b22095a78c70c81902f01c00e91c92262
Mohamedkhames1 <moham01555@gmail.com>
2025-07-01 18:45:39 +0300

    add intialRoute

--- lib/app.dart
+++ lib/app.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
         ),
       ),
       debugShowCheckedModeBanner: false,
+      initialRoute: AppRouter.navigationMenu,
       onGenerateRoute: AppRouter.generateRoute,
     );
   }
